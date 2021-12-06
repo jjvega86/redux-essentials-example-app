7377d1c28667bf05526ef4bfffa14e39c5f14e6f
JJ <jj.vega86@gmail.com>
2021-12-06 05:54:26 -0500

    Added TimeAgo component to PostsList and SinglePostPage to show timestamp of when posts were created relative to current time

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { allPosts } from "./postsSlice";
 import { Link } from "react-router-dom";
 import { PostAuthor } from "./PostAuthor";
+import { TimeAgo } from "./TimeAgo";
 
 export const PostsList = () => {
   const posts = useSelector(allPosts);
@@ -13,7 +14,11 @@ export const PostsList = () => {
     return (
       <article className="post-excerpt" key={post.id}>
         <h3>{post.title}</h3>
-        <PostAuthor userId={post.user} />
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </div>
+
         <p className="post-content">{post.content}</p>
         <Link to={`/posts/${post.id}`} className="button muted-button">
           View Post
