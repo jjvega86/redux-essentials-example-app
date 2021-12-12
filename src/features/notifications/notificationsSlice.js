77fd6a45e01435a276ecf0af6126a6c9566c934f
JJ <jj.vega86@gmail.com>
2021-12-12 04:33:25 -0500

    Added notificationsSlice to project

---
+++ src/features/notifications/notificationsSlice.js
@@ -0,0 +1,16 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import { client } from "../../api/client";
+
+export const fetchNotifications = createAsyncThunk(
+  "notifications/fetchNotifications",
+  async (_, { getState }) => {
+    const allNotifications = selectAllNotifications(getState());
+    const [latestNotification] = allNotifications;
+    const latestTimestamp = latestNotification ? latestNotification.date : "";
+    const response = await client.get(
+      `/fakeApi/notifications?since=${latestTimestamp}`
+    );
+    return response.data;
+  }
+);
