66972472354b9ee58727a6319ac80fc1356861eb
JJ <jj.vega86@gmail.com>
2021-12-11 21:34:36 -0500

    Added conditional rendering for all possible loading states in PostsList.js

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectAllPosts, fetchPosts } from "./postsSlice";
+
+import { Spinner } from "../../components/Spinner";
 import { PostExcerpt } from "./PostExcerpt";
 
 export const PostsList = () => {
   const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts);
   const postStatus = useSelector((state) => state.posts.status);
+  const error = useSelector((state) => state.posts.error);
 
   useEffect(() => {
     if (postStatus === "idle") {
@@ -14,16 +17,24 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch]);
 
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
-  const renderedPosts = orderedPosts.map((post) => {
-    return <PostExcerpt post={post} />;
-  });
+  let content;
+  if (postStatus === "loading") {
+    content = <Spinner />;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => {
+      return <PostExcerpt key={post.id} post={post} />;
+    });
+  } else if (postStatus === "error") {
+    content = <div>{error}</div>;
+  }
+
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
